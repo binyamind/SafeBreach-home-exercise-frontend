00b3706932e19a977a6cd9987bf29650c4f59284
Binyamindeutsch <96825999+Binyamindeutsch@users.noreply.github.com>
2024-04-14 01:34:21 +0300

    fix warpper funcion

--- src/utils/test.utils.tsx
+++ src/utils/test.utils.tsx
@@ -8,7 +8,7 @@ afterEach(() => {
 function customRender(ui: React.ReactElement, options = {}) {
   return render(ui, {
     // wrap provider(s) here if needed
-    wrapper: ({ children }) => children,
+    wrapper: ({ children } ) => children  as any,  
     ...options,
   })
 }
